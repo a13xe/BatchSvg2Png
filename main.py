90707eded6a696fa86b583093fd62939c6764e5c
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2023-03-19 21:15:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# The code uses the os library to iterate through all files in the specified folder. 
+# For each file that ends with .svg, it reads the SVG file using svg2rlg from svglib and renders it to a PNG image using renderPM from reportlab. 
+# The resulting PNG image is saved to the "output" folder folder with the same filename as the SVG file.
+
+
 import os
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
